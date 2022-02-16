c818f2dc596a47e231d88eeb72570e1e83528094
Abhishek kumar <flashtoak@gmail.com>
2022-02-16 15:56:15 +0530

    variables in java

--- sessions/HelloWorld/src/HelloWorld.java
+++ sessions/HelloWorld/src/HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
     public static void main(String [] args){
-        System.out.println("Hello World");
+        System.out.println("Hello Abhishek");
+
+        int myFirstNumber = (5+10)+(5*12);
+        int mySecondNumber = 12;
+        int myTotal = myFirstNumber + mySecondNumber;
+        System.out.println(myTotal);
 
     }
 }
