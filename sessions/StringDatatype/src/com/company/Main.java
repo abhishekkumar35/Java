a03d08852100875b7888ef783af1aa897b54f002
Abhishek kumar <flashtoak@gmail.com>
2022-02-17 09:15:59 +0530

    String datatype

---
+++ sessions/StringDatatype/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // String is not primitive datatype in java it is a class.
+        // String takes series of char.
+        // max characters String can withhold is max value of int.
+
+        String myStr = "hello, yo! this is great.";
+        String intStr = "25";
+        int $int = 25;
+        System.out.println(myStr);
+        System.out.println(intStr);
+        System.out.println(intStr + $int);  // when string is first then java convert int after that string as string and append that"
+
+    }
+}
